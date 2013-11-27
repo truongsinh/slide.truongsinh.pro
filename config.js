4ed62ddadbc7f5a747b6468db99be3a5d4d0ca12
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2013-11-27 13:28:54 +0200

    QR code to the link and linkedin/twitter

--- config.js
+++ config.js
@@ -9,3 +9,22 @@ Reveal.initialize({
   //transition: Reveal.getQueryHash().transition || 'default' // default/cube/page/concave/zoom/linear/fade/none
 });
 hljs.initHighlightingOnLoad();
+
+//
+(function(QRCode, location){
+  // hack image style to get it inline
+  var myqrcodelink = new QRCode("qrcodelink", {
+    text: location.protocol + '//' + location.host + location.pathname,
+    width: 256,
+    height: 256,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.L
+  });
+  var _oDrawing = myqrcodelink._oDrawing;
+  var _fSuccess = _oDrawing._fSuccess;
+  _oDrawing._fSuccess = function(){
+    _fSuccess.apply(this, arguments);
+    this._elImage.style.display = "inline";
+  };
+})(QRCode, location);
